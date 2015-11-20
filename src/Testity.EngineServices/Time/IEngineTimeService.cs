c1e7562283270d0326090df842bbbce7e3a87d8b
Andrew Blakely <andrew.blakely@ymail.com>
2015-11-19 22:16:22 -0600

    Added FixedDelta Timing
    Added FixedDelta Timing. Unreal Engine 4 doesn't provide this by default
    so an additional non-Unity implemented property for determining if it
    has been enabled has been created.

--- src/Testity.EngineServices/Time/IEngineTimeService.cs
+++ src/Testity.EngineServices/Time/IEngineTimeService.cs
@@ -16,8 +16,8 @@ namespace Testity.EngineServices
 		/// <summary>
 		/// Time passed since last frame in seconds.
 		/// Generally depends on <see cref="GlobalTimeScale"/>
-		/// Based on Unity3D's http://docs.unity3d.com/ScriptReference/Time-deltaTime.html
-		/// Based on UnrealEngine 4's https://docs.unrealengine.com/latest/INT/API/Runtime/Engine/Engine/UWorld/GetDeltaSeconds/index.html
+		/// Based on Unity3D's: http://docs.unity3d.com/ScriptReference/Time-deltaTime.html
+		/// Based on UnrealEngine 4's: https://docs.unrealengine.com/latest/INT/API/Runtime/Engine/Engine/UWorld/GetDeltaSeconds/index.html
 		/// </summary>
 		float FrameDeltaTime { get; }
 
@@ -48,5 +48,19 @@ namespace Testity.EngineServices
 		/// Additional Notes: (Some engines provide GameObject/Actor/Entity specific dilation/timescale)
 		/// </summary>
 		float GlobalTimeScale { get; set; }
-	}
+
+		/// <summary>
+		/// Time passed since last fixed frame update in seconds.
+		/// Based on Unity3D's: http://docs.unity3d.com/ScriptReference/Time-fixedDeltaTime.html
+		/// Based on UnrealEngine 4's: https://docs.unrealengine.com/latest/INT/API/Runtime/Core/Misc/FApp/GetFixedDeltaTime/index.html (best I could find)
+		/// </summary>
+		float FixedDeltaTime { get; }
+
+		/// <summary>
+		/// Indicates the state of <see cref="FixedDeltaTime"/>'s computation. If false then <see cref="FixedDeltaTime"/> may not report correct timing.
+		/// Based on Unity3D's: No known implementation. Included for UnrealEngine 4's sake.
+		/// Based on UnrealEngine 4's: https://docs.unrealengine.com/latest/INT/API/Runtime/Core/Misc/FApp/UseFixedTimeStep/index.html
+		/// </summary>
+		bool FixedTimeStepEnabled { get; }
+    }
 }
