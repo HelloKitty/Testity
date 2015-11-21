82ed114e54744ed9ca4f2430135eec05cb8d3136
Andrew Blakely <andrew.blakely@ymail.com>
2015-11-21 15:56:39 -0600

    Attempt to fix Travis for NUnit 3

--- tests/RunTests.sh
+++ tests/RunTests.sh
@@ -1,3 +1,3 @@
 nunit-console ./Testity.EngineServices.Tests/bin/Debug/Testity.EngineServices.Tests.dll
 nunit-console ./Testity.EngineComponents.Tests/bin/Debug/Testity.EngineComponents.Tests.dll
-nunit-console ./Testity.EngineMath.Tests/bin/Debug/Testity.EngineMath.Tests.dll
+nunit-console ./Testity.EngineMath.Tests/bin/Debug/Testity.EngineMath.Tests.dll -domain=None
