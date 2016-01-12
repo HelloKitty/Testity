c91665a4274f2b4314bc3ff8b33d65f4519f2789
Andrew Blakely <andrew.blakely@ymail.com>
2016-01-11 22:46:03 -0600

    TypeMapper for IEngineObjects to UnityObjects

--- src/Testity.BuildProcess.Unity3D/TypeMapping/UnityObjectTypeRelationalMapper.cs
+++ src/Testity.BuildProcess.Unity3D/TypeMapping/UnityObjectTypeRelationalMapper.cs
@@ -3,19 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Fasterflect;
+using Testity.Common;
+using Testity.EngineComponents;
 
 namespace Testity.BuildProcess.Unity3D
 {
-	public class UnityObjectTypeRelationalMapper : ITypeRelationalMapper
+	public class EngineObjectTypeRelationalMapper : ITypeRelationalMapper
 	{
 		public Type ResolveMappedType(Type typeToFindRelation)
 		{
-			//If this is not a UnityEngine.Object then return null
-			//Otherwise we have found a Type that is a Unity object and need more information for mapping.
-			if (!typeToFindRelation.IsSubclassOf(typeof(UnityEngine.Object)))
+			//If this isn't an IEngineObject then we're unable to map it
+			if (!typeof(IEngineObject).IsAssignableFrom(typeToFindRelation))
 				return null;
-			else
-				return null;
-		}
+
+			//We need to find the [EngineSerializableMapsToType] attribute that is for UnityEngine
+			EngineSerializableMapsToTypeAttribute mapInfo = typeToFindRelation.Attributes<EngineSerializableMapsToTypeAttribute>()
+				.FirstOrDefault(x => x.EngineType == EngineType.Unity3D);
+
+			//Possible that we don't have this Engine type setup it.
+			//Though this is unlikely. If an IEngineObject exists it's very likely its been mapped with metadata
+			if (mapInfo == null)
+				throw new InvalidOperationException("Unable to handle " + nameof(IEngineObject) + " type: " + typeToFindRelation.ToString());
+
+			return mapInfo.ConcreteEngineType;
+        }
 	}
 }
